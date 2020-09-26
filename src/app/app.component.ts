0d30b65e9187a2302e6c1e8647c0744295dddaf2
nimaytenzin <nimaytenzin@gmail.com>
2020-09-26 18:42:06 +0600

    web mapp added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'real-time-water-monitoring';
+  private map;
+
+  constructor() { }
+
+  ngOnInit(): void {
+     this.map = L.map('map').setView([27.472940, 89.634361], 16);
+    const tiles = L.tileLayer('http://mt0.google.com/vt/lyrs=s&hl=en&x={x}&y={y}&z={z}', {
+      maxZoom:20,
+    });
+    tiles.addTo(this.map)
+    
+  }
 }
+
+
